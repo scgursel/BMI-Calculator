f03bd1d6203137aebfe7213eba744eb22211a5af
scgursel <servetcan99@outlook.com>
2023-08-22 02:16:01 +0300

    Some features.

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Servet Can Gürsel on 22.08.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
